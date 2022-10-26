116ff65e14d9a5bb52b9390fca84913138050c7d
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-10-26 17:26:02 +0300

    Hacking with Swift Day Two

--- IntroductionToSwift.playground/Contents.swift
+++ IntroductionToSwift.playground/Contents.swift
@@ -28,6 +28,88 @@ print(multiLineBreakString2)
 var pi: Double = 3.14
 var awesome: Bool = true
 
+//MARK: - Day 2
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+for member in beatles {
+    print("\(member)")
+}
+
+// Sets collection of values just like arrays
+//except they have two diffrences
+// First items aren't stored in any order
+// They storing randomly
+// Second all items must be unique
+
+
+let colors = Set(["Green", "Black", "White"])
+let colors2 = Set(["red","green", "blue", "red", "blue"])
+
+//Tuple
+var famousName = (first : "Taylor", last: "Swift")
+famousName.0
+famousName.last
+
+//Dictioneries
+var heights: [String : Double] = [
+    "Taylor Swift" : 1.78,
+    "Ed Sheeran" : 1.73
+]
+
+heights["Ed Sheeran"]
+
+let favoriteIceCream: [String:String] = [
+    "Paul": "Chocolate",
+    "Sophie": "Vanilla"
+]
+favoriteIceCream["Paul"]
+favoriteIceCream["Charlotte"]
+favoriteIceCream["Charlotte", default: "Unkown"]
+
+//Enums
+let result = "Failure"
+let result2 = "Failed"
+let result3 = "Fail"
+
+enum Result{
+    case succses
+    case failure
+}
+
+let result4 = Result.failure
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: String)
+}
+
+let talking = Activity.talking(topic: "Football")
+
+enum Planets: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planets(rawValue : 2)
+
+enum Planets2: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let venus = Planets2(rawValue : 2)
 
 
 var ageDescription = "Ali's age is \(age)"
