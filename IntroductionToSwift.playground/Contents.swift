52f93c0a18a7c25fb28494edb85ac46d86be718c
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-10-28 01:03:36 +0300

    Hacking with Swift Day 3

--- IntroductionToSwift.playground/Contents.swift
+++ IntroductionToSwift.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// MARK: - Day 1
+
 var str = "Hello, playground"
 
 str="Goodbye"
@@ -116,3 +118,95 @@ var ageDescription = "Ali's age is \(age)"
 
 // let can't change later
 let taylor = "Swift"
+
+
+//MARK: - Day 3
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divides = firstScore / secondScore
+
+let remainder = 13 % secondScore
+
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers Gonna"
+let action = fakers + " Fake"
+
+
+let firstHalf = ["Paul", "John"]
+let secondHalf = ["George", "Ringo"]
+let beatle2 = firstHalf + secondHalf
+
+var score = 95
+    score -= 90
+
+var qoute = "The rain in Spain falls mainly on the"
+    qoute += "Spaniards"
+
+let thirdScore = 6
+let fourthScore = 4
+
+thirdScore == fourthScore
+thirdScore != fourthScore
+thirdScore < fourthScore
+thirdScore >= fourthScore
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces - Lucky")
+} else if firstCard + secondCard == 21{
+    print("Black Jack")
+} else {
+    print("Regular Cards")
+}
+
+let age1 = 12
+let age2 = 21
+
+if (age1 > 18 && age2 > 18) {
+    print("Both are over 18")
+}
+
+if (age1 > 18 || age2 > 18) {
+    print("One of'em is over 18")
+}
+
+let thirdCard = 11
+let fourthCard = 10
+
+print(thirdCard == fourthCard ? "Cards are the same": "Cards are different")
+
+let weather = "sunny"
+
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap warm up")
+case "sunny":
+    print("Wear sunscree")
+    fallthrough
+default:
+    print("Enjoy your day")
+}
+
+
+let point = 85
+
+switch point {
+case 0..<50:
+    print("You failed badly!")
+case 50..<85:
+    print("You did OK!")
+default:
+    print("You did great!")
+}
