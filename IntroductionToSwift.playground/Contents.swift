891e73569083e3ee784d4f58af507cd3f672c933
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-10-29 15:08:16 +0300

    Hacking with Swift Day 4

--- IntroductionToSwift.playground/Contents.swift
+++ IntroductionToSwift.playground/Contents.swift
@@ -210,3 +210,89 @@ case 50..<85:
 default:
     print("You did great!")
 }
+
+
+//MARK: - Day 4
+
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+print("Player gonna")
+
+for _ in 1...5 {
+    print("Play")
+}
+
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number+=1
+}
+
+print("Ready or not, here I come!")
+
+number = 1
+
+repeat {
+    print(number)
+    number+=1
+}while number <= 20
+
+print("Ready or not here I come!")
+
+repeat {
+    print("This is false")
+} while false
+
+var countDown = 10
+
+while countDown>=0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now")
+        break
+    }
+    countDown-=1
+}
+print("Blast off!")
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+for i in 1...10 {
+    if i % 2 == 1 {
+        continue
+    }
+    print(i)
+}
+
+var counter = 0
+
+while true {
+    print(" ")
+    counter += 1
+
+    if counter == 273 {
+        break
+    }
+}
