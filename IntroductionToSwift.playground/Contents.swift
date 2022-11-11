ef343f9ee1a181b22c3752976b9f2345bd4cf5e6
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-11-11 17:53:51 +0300

    Hacking with Swift Day 8

--- IntroductionToSwift.playground/Contents.swift
+++ IntroductionToSwift.playground/Contents.swift
@@ -504,3 +504,94 @@ func travel6(action: (String, Int) -> String){
 travel6 {
     "I'm going to \($0) at \($1) miles per hour"
 }
+
+// MARK: - Day 8
+
+struct Sport {
+    var name: String
+}
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+    
+    
+    var olympicStatus: String {
+        if isOlympicSport{
+            return "\(name) is an Olympic Sport"
+        }else {
+            return "\(name) is not an Olympic Sport"
+        }
+    }
+}
+
+var chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+
+print(chessBoxing.olympicStatus)
+
+struct Progress {
+    var task: String
+    var amount: Int{
+        didSet{
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading Data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct City {
+    var name: String
+    var population: Int
+    
+    func collectTaxes() -> Int{
+        return population * 1000
+    }
+}
+
+var london = City(name: "London", population: 9_000_000)
+london.collectTaxes()
+
+
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+let string = "Do or do not, there is no try"
+let intInString = "759"
+
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+print(intInString.sorted())
+
+
+var toys = ["Woody"]
+
+print(toys.count)
+
+toys.append("Buzz")
+
+toys.firstIndex(of: "Buzz")
+
+print(toys.sorted())
+
+toys.remove(at: 0)
+
+var fibonacci = [1, 1, 2, 3, 5, 8]
+fibonacci.sorted() == [1, 2, 3, 5, 8]
+
+
