eb4716cdb28820d1bc179f74731024bcf4745a2c
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-11-17 14:06:37 +0300

    Hacking with Swift Day 10

--- IntroductionToSwift.playground/Contents.swift
+++ IntroductionToSwift.playground/Contents.swift
@@ -668,3 +668,95 @@ struct Person4 {
 let ed2 = Person4(id: "12345")
 
 print(ed2.identify())
+
+// MARK: - Day 10
+
+class Dog{
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+    
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodel")
+
+
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+    override func makeNoise() {
+        print("Yip!")
+    }
+}
+
+let poppy2 = Poodle(name: "Poppy")
+print(poppy2.name)
+print(poppy2.breed)
+
+poppy.makeNoise()
+poppy2.makeNoise()
+
+final class Dog2 {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+//class pitbull: Dog2{
+//    Inheritance from a final class 'Dog2'
+//}
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justing Bieber"
+print(singer.name)
+
+class Person5 {
+    var name = "John Doe"
+    
+    init() {
+        print("\(name) is alive!")
+    }
+    
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person5()
+    person.printGreeting()
+}
+
+class Singer2 {
+    let name = "Taylor Swift"
+}
+
+let taylor3 = Singer2()
+//taylor3.name = "Ed Sheeran"
+print(taylor3.name)
+
+
+
