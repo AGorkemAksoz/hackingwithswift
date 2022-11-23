5810e9e3de43204e1a39e5a0c5a9cb89932c6bb2
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2022-11-23 15:45:15 +0300

    Hacking with Swift Project 1 (Day 16)

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Ali Görkem Aksöz on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                // this is the picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        print(pictures.count)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(pictures.count)
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
+
